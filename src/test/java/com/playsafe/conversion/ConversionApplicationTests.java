17a073229dc4f1fe4f97d9674b0549baca4babdf
Peter Togara <petertogara@gmail.com>
2020-12-13 13:40:40 +0200

    initial commit

---
+++ src/test/java/com/playsafe/conversion/ConversionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playsafe.conversion;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConversionApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
