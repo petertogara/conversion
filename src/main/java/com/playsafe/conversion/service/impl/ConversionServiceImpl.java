fca9c79cd781759bfa81d84248941230bce8b338
Peter Togara <petertogara@gmail.com>
2020-12-13 14:37:37 +0200

    Added implementation

--- src/main/java/com/playsafe/conversion/service/impl/ConversionServiceImpl.java
+++ src/main/java/com/playsafe/conversion/service/impl/ConversionServiceImpl.java
@@ -1,6 +1,7 @@
 package com.playsafe.conversion.service.impl;
 
 import com.playsafe.conversion.service.api.ConversionService;
+import com.playsafe.conversion.utils.ConversionConstants;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,21 +9,21 @@ public class ConversionServiceImpl implements ConversionService {
 
     @Override
     public double kelvinToCelsius(double inputToBeConverted) {
-        return 0;
+        return inputToBeConverted - ConversionConstants.KELVIN_TO_CELCIOUS_RATE;
     }
 
     @Override
     public double celsiusToKelvin(double inputToBeConverted) {
-        return 0;
+        return inputToBeConverted + ConversionConstants.KELVIN_TO_CELCIOUS_RATE;
     }
 
     @Override
     public double milesToKilometers(double inputToBeConverted) {
-        return 0;
+        return inputToBeConverted * ConversionConstants.MILE_TO_KILOMETER_RATE;
     }
 
     @Override
     public double kilometersToMiles(double inputToBeConverted) {
-        return 0;
+        return inputToBeConverted * ConversionConstants.KILOMETER_TO_MILE_RATE;
     }
 }
